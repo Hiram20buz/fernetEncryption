2b67b80e0730b0209a5031befcfdee8ead092fd7
Hiram20buz <l20211847@tectijuana.edu.mx>
2023-12-29 10:52:38 -0800

    test_encrypt_file

--- steps/test_commonSteps.py
+++ steps/test_commonSteps.py
@@ -12,6 +12,18 @@ class TestCommonSteps(unittest.TestCase):
     def test_readKey(self):
         self.assertIsNotNone(commonSteps.readKey())
 
+    def test_encrypt_file(self):
+        commonSteps.generateKey()
+        key = commonSteps.readKey()
+        a = open("testsEnvironment/file.txt").read()
+        commonSteps.encrypt_file("testsEnvironment/file.txt", key)
+        b = open("testsEnvironment/file.txt").read()
+        self.assertNotEqual(a, b)
+        commonSteps.encrypt_file("testsEnvironment/file.txt", key, False)
+        c = open("testsEnvironment/file.txt").read()
+        self.assertEqual(a, c)
+        
+
 
 if __name__ == '__main__':
     unittest.main()
