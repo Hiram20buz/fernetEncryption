6383b3f4c2807b3331cd3a4da0689e731eedf790
Hiram20buz <l20211847@tectijuana.edu.mx>
2024-01-04 13:35:48 -0800

    copy,run and delete

--- steps/b.py
+++ steps/b.py
@@ -4,3 +4,13 @@ import shutil
 src="a.py"
 dst="delete.py"
 shutil.copy(src,dst)
+
+
+try:
+    with open(dst, 'r') as file:
+        script_contents = file.read()
+        exec(script_contents)
+except FileNotFoundError:
+    print(f"File '{dst}' not found.")
+except Exception as e:
+    print(f"An error occurred while executing '{dst}': {e}")
